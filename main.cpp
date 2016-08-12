532ae457ce2e9c044a71e675bb8a1962c7c74b6f
adashbob <bobodiallobe@gmail.com>
2016-08-12 18:29:03 +0000

    Test Pattern Singleton

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
-#include <Singleton/Singleton.h>
-#include <Singleton/Connexion.h>
-
-using namespace std;
 
 int main() {
-    Connexion& obj1 = Singleton<Connexion>::getInstance();
-    Connexion& obj2 = Singleton<Connexion>::getInstance();
-    Connexion& obj3 = Singleton<Connexion>::getInstance();
-
-    cout<< " obj1 : " << &obj1 << endl;
-    cout<< " obj1 : " << &obj2 << endl;
-    cout<< " obj1 : " << &obj3 << endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
