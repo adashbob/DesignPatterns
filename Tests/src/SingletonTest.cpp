9b0d26bc17fdc60b125c1a139c8fda0137e30878
adashbob <bobodiallobe@gmail.com>
2016-08-12 20:52:55 +0000

    lenvo Test Pattern Singleton

--- Tests/src/SingletonTest.cpp
+++ Tests/src/SingletonTest.cpp
@@ -2,9 +2,9 @@
 #include <Singleton/Connexion.h>
 
 TEST(SingletonTest, ConstructorCalledOnce){
-    Connexion& obj1 = Singleton<Connexion>::getInstance();
-    Connexion& obj2 = Singleton<Connexion>::getInstance();
-    Connexion& obj3 = Singleton<Connexion>::getInstance();
+    Singleton<Connexion>& obj1 = Singleton<Connexion>::getInstance();
+    Singleton<Connexion>& obj2 = Singleton<Connexion>::getInstance();
+    Singleton<Connexion>& obj3 = Singleton<Connexion>::getInstance();
 
     EXPECT_EQ(&obj1, &obj2);
 //    EXPECT_NE(&obj2, &obj3);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <Singleton/Singleton.h>
+#include <Singleton/Connexion.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Singleton<Connexion>& obj1 = Singleton<Connexion>::getInstance();
+    Singleton<Connexion>& obj2 = Singleton<Connexion>::getInstance();
+    Singleton<Connexion>& obj3 = Singleton<Connexion>::getInstance();
 }
